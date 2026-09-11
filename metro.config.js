9297cbfcc1a61879d5a2877bd932a5512722f217
Nguyen Gia Tin <tin952005@gmail.com>
2026-09-12 00:52:23 +0700

    Sửa lỗi Metro Config, khôi phục cấu hình mặc định

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,2 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = withNativeWind(config, { input: "./global.css" });
+const { getDefaultConfig } = require('expo/metro-config');
+module.exports = getDefaultConfig(__dirname);
